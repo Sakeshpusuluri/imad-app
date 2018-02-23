34fbc9b822c02c288fb3fe65d84919153a5c691f
Sakeshpusuluri <36641361+Sakeshpusuluri@users.noreply.github.com>
2018-02-24 00:16:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ app.get('/', function (req, res) {
 app.get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'article-one.html'));
 });
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+ app.get('/article-three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'article-three.html'));
+ });
+ 
 app.get('/article-two',function(req,res){
     res.send('Article two is requested and will be served here');
 });
