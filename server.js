3fadfdb8ea201f9cec922c68b7c874a7ed74ac2b
Sakeshpusuluri <36641361+Sakeshpusuluri@users.noreply.github.com>
2018-02-24 07:28:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,61 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
+var express=require('express');
+var morgan=require('morgan');
+var path=require('path');
+var app=express();
 app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/',function(req,res){
+    res.sendFile(path,join(_dirname,'ui',index.html));
 });
+var articleone={
+    title:'Articleone',
+    heading:'Article-one Console',
+    date:'Feb 24 2018',
+    content:`
+            <p>I want to be a traveller.. So, Frikingbad.........</p>
+            <p> I want to be a traveller.. So, Frikingbad.........</p>
+            <p> I want to be a traveller.. So, Frikingbad.........</p>
+    `
+};
+function createtemplate(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.content;
+    var date=data.data;
+    
+var htmltemplate = `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width-device-width; intial-scale=1"/>
+       <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+         <div>
+        <a href="/">HOME</a>
+        <hr>
+        </div>
+        <div>
+        <h1>${heading}</h1>
+        </div>
+        <div>
+        <p> ${Date}</p>
+        </div>
+      
+        <div>
+           ${content}
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return htmltemplate;
+}
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'article-one.html'));
+    res.send(createtemplate(articleone));
 });
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
